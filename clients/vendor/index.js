e46316eb8724e226b66c50924c7433342fdde4ed
Joe Davitt <joseph.j.davitt@gmail.com>
2023-02-28 23:36:58 +0000

    implement socket

--- clients/vendor/index.js
+++ clients/vendor/index.js
@@ -1,39 +1,26 @@
 'use strict';
 
-const eventPool = require('../../eventPool');
+// const eventPool = require('../../eventPool');
 const handler = require('./handler');
 
 const { io } = require('socket.io-client');
 
-const socket = io('http://localhost:3001/caps');
+const socket = io.connect('http://localhost:3001/caps');
 
-eventPool.on('VENDOR', (store) => {
-  setTimeout(() => {
-    handler(store);
-  }, 1000);
-});
+let store = '1-206-flowers';
 
-eventPool.on('delivered', (payload) => {
+socket.emit('JOIN', store);
+
+
+setInterval(() => {
+  handler(store);
+}, 5000);
+
+
+socket.on('delivered', (payload) => {
   console.log(`VENDOR: Thank you for delivering ${payload.orderID}`);
+  process.exit(0);
 });
 
 
-// var Chance = require('chance');
-// var chance = new Chance();
 
-
-// const newPackage = (store) => {
-//   setInterval(() => {
-
-//     const payload = {
-//       store: store,
-//       orderID: chance.guid(),
-//       customer: chance.name(),
-//       address: chance.address(),
-//     };
-//     eventPool.emit('pickup', payload);
-//   }, 5000);
-// }  
-
-
-// module.exports = newPackage;
